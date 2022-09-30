3504912c42c22040f473555e95a5932b2fedd065
RiverRay <laolei@forkway.cn>
2022-09-30 15:31:59 +0800

    feat: 自定义 preset css

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import ArcoVueIcon from '@arco-design/web-vue/es/icon'
 import App from './App.vue'
 import './styles/main.css'
 import 'uno.css'
-import '@unocss/reset/tailwind.css'
 // should be less , if want to custom theme
 import '@arco-themes/vue-taolu-publishing-platform/index.less'
 
