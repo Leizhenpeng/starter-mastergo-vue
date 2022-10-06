efbe9c9414045c5ff57c6cd3937e505681405e30
RiverRay <laolei@forkway.cn>
2022-10-06 11:29:07 +0800

    chore: 引入 emit hook

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import './styles/main.css'
 import 'uno.css'
 // should be less , if want to custom theme
 // import '@arco-themes/vue-taolu-publishing-platform/index.less'
-
+import '~/listen.emit'
+import '~/send.emit'
 const app = createApp(App)
 app.mount('#app')
